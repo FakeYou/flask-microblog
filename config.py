94640b30d19d5c4ef0eea72bdfa94a17ded1fc3a
Andre Nanninga <ag.nanninga@gmail.com>
2015-09-09 20:55:36 +0200

    finished part 4: databases

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+print(basedir)
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'nsssssssss789rhga9ohOAHY789sd hAsdg9d& B(ASH&*DogH789yasdtOAI7fga'
 
@@ -5,3 +10,6 @@ OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
