ba32dc5a277415ed05f3a9bda8e2c3c434d78c62
Andre Nanninga <ag.nanninga@gmail.com>
2015-09-09 20:19:50 +0200

    finished part 3

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'nsssssssss789rhga9ohOAHY789sd hAsdg9d& B(ASH&*DogH789yasdtOAI7fga'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
